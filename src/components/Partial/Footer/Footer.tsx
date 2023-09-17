20d88fefccdfb2d2a13507c8f32b0512a0a8cdec
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-09-17 23:52:29 +0300

    add telegram link

--- src/components/Partial/Footer/Footer.tsx
+++ src/components/Partial/Footer/Footer.tsx
@@ -6,7 +6,10 @@ export const Footer = () => {
   return (
     <div className='footer'>
         <div className="footer__container _container">
-            <h3 className='footer__title'>OktenSchool students by Artiom Dumchykov</h3>
+            <h3 className='footer__title'>
+              OktenSchool students by Artiom Dumchykov 
+              (telegram: <a href="https://t.me/artemdumchykov" className='teleg-link' target='_blank' >@artemdumchykov</a>)
+            </h3>
         </div>
     </div>
   )
